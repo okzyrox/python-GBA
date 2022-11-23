d707e6cfa30ca733e90ff3f161317df4350a0799
ZyroX <79484310+okzyrox@users.noreply.github.com>
2022-11-23 22:32:08 +0000

    create base for emulator file loading
    not functional, more of a representation of thought from someone completely inexperienced in low-level device emulation

---
+++ emu.py
@@ -0,0 +1,12 @@
+import cpu, mem, cfg
+
+instructions = []
+
+def emu(filepath):
+    file = filepath
+    while True:
+        while len(instructions) >= 0:
+            for i in instructions:
+                return i
+        
+        # do later when less tired
