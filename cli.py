c7efd7073452e7a040b5f5f72d10ed0dc506b03f
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2022-11-25 19:52:17 -0600

    Some CPU instructions and memory tests

--- cli.py
+++ cli.py
@@ -1,4 +1,9 @@
 from pyGBA.__main__ import main, test
+from os import environ
+DEBUG_MODE = environ.get('PYGBA_DEBUG')
 
 if __name__ == '__main__':
-    test()
+    if DEBUG_MODE == '1':
+        test() # Run tests
+    else:
+        main() # Main
--- pyGBA/tests/testCore/testMem.py
+++ pyGBA/tests/testCore/testMem.py
@@ -1,4 +1,9 @@
 from . import *
 
 def memTest():
-    return RAM()
+    ram = RAM()
+    log('RAM Randomization Test:', 2)
+    log('IO Memory hashed before randomization: %s' % hash(str(ram.IO.mem)), 2)
+    ram.IO.randomize(0, len(ram.IO.mem))
+    log('IO Memory hashed after randomization: %s' % hash(str(ram.IO.mem)), 2)
+    return ram
