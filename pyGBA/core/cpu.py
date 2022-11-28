945f4f1620ff523356bbef1de580bc6f18882c6a
ZyroX <79484310+okzyrox@users.noreply.github.com>
2022-11-28 00:03:10 +0000

    testing new cpu operand sys + TST impl with this

--- pyGBA/core/cpu.py
+++ pyGBA/core/cpu.py
@@ -1,5 +1,13 @@
 from . import *
 
+# 'S' == Set Condition Codes
+# 'Rn' == 1st Operand Register
+# 'Op2' == 2nd Operand Register
+# 'Rd' == Destination Register
+
+# reformat to match actual names or??????
+# https://iitd-plos.github.io/col718/ref/arm-instructionset.pdf
+
 class String: # Deprecated, delete this soon
     class Instruction:
         # Move
@@ -21,6 +29,14 @@ class String: # Deprecated, delete this soon
         def ADD(instruction):
             list = CPU.Execution.parse(instruction)
             return None
+
+class Op(): # 'new' replacement for op2 / op1 since they need set conditions codes, move loc if needed
+    def __init__(self, count):
+        self.set_condition_code = 0 # int_to_bytes needed???, i dont know assembly
+        self.op_num = 0 # change to either 1 or 2 on creation
+
+        # self.flag_* = *
+
 
 class CPU():
     def __init__(self):
@@ -62,8 +78,25 @@ class CPU():
     def BIC(self, Rd, Rn, Op2, *, cond = None, S = None):
         self.registers[Rd] = self.int_to_bytes(self.bytes_to_int(self.registers[Rn]) & (~self.register_to_int(Op2) & 0xFFFFFFFF))
 
-    def TST(self, Rn, Op2, *, cond = None, P = None):
-        pass
+    def TST(self, Rn, Op2, *, cond = None, P = None, S = None):
+        # new sys???
+        S = 1
+        Rn = Op(count=1) # temp, waiting approval + read topmost comment & its PDF + 'Op' class
+        Op2 = Op(count=2) # temp, waiting approval + read topmost comment & its PDF + 'Op' class
+
+        # will probably move outside of func to remove redefining it every time
+
+        if isinstance(cond, int):
+            if cond == None:
+                Rn.set_condition_code, Op2.set_condition_code = self.int_to_bytes(1) # set cond code to '1'
+            elif cond != None:
+                Rn.set_condition_code, Op2.set_condition_code = self.int_to_bytes(cond)
+        else:
+            return 0
+        
+        # cant wait for all this code to do absolutely nothing if i went wrong somewhere; more likely since this is stuff
+        # that i barely comprehand lol
+
 
     def TEQ(self, Rn, Op2, *, cond = None, P = None):
         pass
